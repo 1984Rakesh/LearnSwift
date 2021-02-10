4ca514acb5cfa8c9464a8761bfdb89b976970263
Rakesh Patole <rakeshp@ohumhealthcare.com>
2021-02-10 11:50:56 +0530

    added abstract factory pattern

--- LearnSwift.playground/Pages/CreationalDesignPatterns.xcplaygroundpage/Contents.swift
+++ LearnSwift.playground/Pages/CreationalDesignPatterns.xcplaygroundpage/Contents.swift
@@ -1,3 +1,6 @@
+//: **Creational Desing Patterns**
+//: * All patterns here are related to how objects are created.
+//:
 //: **Singletone** :-
 //: * Only one instance of the class is created thorught the application life cyle
 //: * let someClass = SomeClass() wont work since init is private
@@ -13,12 +16,13 @@ class SomeClass {
         print("Hello!!")
     }
 }
-
 SomeClass.manager.printDescription()
 
+//:
 //: **Abstract Factory**
 //: * One Abstract Class called as Abstarct Factor to create group of related objects
 //: * Mutliple concreate Factories of Abstarct Factory class to create particular group of related objects
+//: * e.g. TIU Teplate, same template but look and feel was different for CWF and Scores.
 class Window {
     func printDescription() {
         print("Base Window!!")
@@ -36,14 +40,37 @@ class MacOSWindow : Window {
         print("MacOS Window")
     }
 }
+
+class Button {
+    func printDescription() {
+        print("Base Button!!")
+    }
+}
+
+class iOSButton : Button{
+    override func printDescription() {
+        print("iOS Button")
+    }
+}
+
+class MacOSButton : Button {
+    override func printDescription() {
+        print("MacOS Button")
+    }
+}
 //: * Abstract Factor class that specifies the list of items to be created
 protocol AbstractFactory {
     func createWindow() -> Window
+    func createButton() -> Button
 }
-
+//: * Concrete classes responsible for creation of the actula objects
 class iOSFactory : AbstractFactory {
     func createWindow() -> Window {
         return iOSWindow()
+    }
+    
+    func createButton() -> Button {
+        return iOSButton()
     }
 }
 
@@ -51,4 +78,15 @@ class MacOSFactory : AbstractFactory {
     func createWindow() -> Window {
         return MacOSWindow()
     }
+    
+    func createButton() -> Button {
+        return MacOSButton()
+    }
 }
+
+var button : Button;
+button = iOSFactory().createButton();
+button.printDescription()
+
+button = MacOSFactory().createButton();
+button.printDescription()
