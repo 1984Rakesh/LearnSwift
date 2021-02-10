638b0ef2ad9d694be6dcf676da8b2fc3a8a676cf
Rakesh Patole <rakeshp@ohumhealthcare.com>
2021-02-10 11:51:12 +0530

    updated general questions

--- LearnSwift.playground/Pages/GeneralInterviewQuestions.xcplaygroundpage/Contents.swift
+++ LearnSwift.playground/Pages/GeneralInterviewQuestions.xcplaygroundpage/Contents.swift
@@ -33,10 +33,10 @@
 //:
 //: **Desing Patters :-**
 //: 1. Explain Factory method ? difference between factory and abstract factory ?
-//: 2. Singelton in swift ? pure singleton ? you should not be able to create another instance of that class. [Sample](DesignPatterns)
-//: 3. explain MVVM in details ?
+//: 2. [Singelton](DesignPatterns) in swift ? pure singleton ? you should not be able to create another instance of that class. Sample
+//: 3. explain M-V-VM in details ?
 //: 4. explain VIPER in details ?
-//: 5. What logic will go where w.r.t MVVM ? two textfield one for username the other for password and on button ? validation on password field ? on click of button authroisation of user with api call? what will go where with the architecture of the system? [Refere M-V-VM](MVVM)
+//: 5. What logic will go where w.r.t M-V-VM ? two textfield one for username the other for password and on button ? validation on password field ? on click of button authroisation of user with api call? what will go where with the architecture of the system? [Refere M-V-VM](MVVM)
 //:
 //: **OOPS :-**
 //: 1. what are four prinicples of OOPS? and explain each ?
